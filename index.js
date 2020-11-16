7b388f828e4797dcb23737ca54cfcf416a48f369
metrio <dlima.nyit@gmail.com>
2020-11-16 18:16:34 -0500

    commit tree 6aabc4e0620a03c456305e6dd6b087020432d8f0

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!"
+newHeader.className = "victory"
